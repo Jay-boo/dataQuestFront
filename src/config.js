a2778534219260ad32c097da950f6c72f3f21c2d
jay <jeanalainpro2@gmail.com>
2024-02-19 16:19:10 +0100

    intercept var env in img

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@ import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 const env = runtimeEnv( );
 console.log("---------------GET CONFIG----------------");
-console.log("REACT API HOST :",process.env.API_URL);
-console.log("REACT API PORT :",process.env.API_PORT);
+console.log("REACT API HOST :",env.API_URL);
+console.log("REACT API PORT :",env.API_PORT);
 const config = {
   // apiBasePath: 'http://localhost:80' ,
-  apiBasePath: `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}` ,
+  apiBasePath: `${env.API_URL}:${env.API_PORT}` ,
 
 };
 
