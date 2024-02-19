30a80185fa759be2b13492d1fc358ad8cfa4ec7c
jay <jeanalainpro2@gmail.com>
2024-02-19 15:16:46 +0100

    firstly create repo

--- deploy/cloudrun.tf
+++ deploy/cloudrun.tf
@@ -1,52 +1,52 @@
 
-resource "google_cloud_run_v2_service" "react-frontend" {
-  project  = var.project_id
-  name     = var.service_name
-  location = var.region
-  ingress = "INGRESS_TRAFFIC_ALL"
-
-  template {
-    service_account = google_service_account.sa-cloudrun-frontend.email
-    containers {
-      image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.artifact_repo_name}/${var.image_name}:latest"
-      
-      resources {
-        limits = {
-          cpu    = "1"
-          memory = "256Mi"
-        }
-        # Determines whether CPU should be throttled or not outside of requests
-        cpu_idle = true
-      }
-      liveness_probe {
-          http_get {
-            path = "/"
-          }
-          initial_delay_seconds= 3600 
-          period_seconds=1000
-          timeout_seconds=1
-      }
-      ports{
-        container_port = 3000
-      }
-      env { 
-          name = "API_URL"
-          value = var.api_url
-      }
-      env { 
-          name = "API_PORT"
-          value = var.api_port
-      }
-    }
-  }
-}
-
-resource "google_cloud_run_service_iam_binding" "default" {
-  project  = var.project_id
-  location = google_cloud_run_v2_service.react-frontend.location
-  service  = google_cloud_run_v2_service.react-frontend.name
-  role     = "roles/run.invoker"
-  members = [
-    "allUsers"
-  ]
-}
+# resource "google_cloud_run_v2_service" "react-frontend" {
+#   project  = var.project_id
+#   name     = var.service_name
+#   location = var.region
+#   ingress = "INGRESS_TRAFFIC_ALL"
+#
+#   template {
+#     service_account = google_service_account.sa-cloudrun-frontend.email
+#     containers {
+#       image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.artifact_repo_name}/${var.image_name}:latest"
+#       
+#       resources {
+#         limits = {
+#           cpu    = "1"
+#           memory = "256Mi"
+#         }
+#         # Determines whether CPU should be throttled or not outside of requests
+#         cpu_idle = true
+#       }
+#       liveness_probe {
+#           http_get {
+#             path = "/"
+#           }
+#           initial_delay_seconds= 3600 
+#           period_seconds=1000
+#           timeout_seconds=1
+#       }
+#       ports{
+#         container_port = 3000
+#       }
+#       env { 
+#           name = "API_URL"
+#           value = var.api_url
+#       }
+#       env { 
+#           name = "API_PORT"
+#           value = var.api_port
+#       }
+#     }
+#   }
+# }
+#
+# resource "google_cloud_run_service_iam_binding" "default" {
+#   project  = var.project_id
+#   location = google_cloud_run_v2_service.react-frontend.location
+#   service  = google_cloud_run_v2_service.react-frontend.name
+#   role     = "roles/run.invoker"
+#   members = [
+#     "allUsers"
+#   ]
+# }
