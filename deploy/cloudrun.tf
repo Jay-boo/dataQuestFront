4076457e40aedccc60eb7460475366eab08d495e
jay <jeanalainpro2@gmail.com>
2024-02-19 15:44:20 +0100

    memory limit fornt end

--- deploy/cloudrun.tf
+++ deploy/cloudrun.tf
@@ -13,7 +13,7 @@ resource "google_cloud_run_v2_service" "react-frontend" {
       resources {
         limits = {
           cpu    = "1"
-          memory = "256Mi"
+          memory = "1Gio"
         }
         # Determines whether CPU should be throttled or not outside of requests
         cpu_idle = true
