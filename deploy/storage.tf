1c6266a8ff378b4d7a141ce84691202705d5e248
jay <jeanalainpro2@gmail.com>
2024-02-22 14:33:44 +0100

    destroy tf resources

--- deploy/storage.tf
+++ deploy/storage.tf
@@ -26,41 +26,41 @@ resource "google_storage_bucket_object" "app" {
   source = data.archive_file.function_dist.output_path
   bucket = google_storage_bucket.app.name
 }
-resource "google_app_engine_standard_app_version" "latest_version" {
-
-  version_id = 1
-  service    = "default"
-  runtime    = "nodejs20"
-  project= var.project_id
-
-  entrypoint {
-    shell = " npm start"
-  }
-  env_variables = {
-    REACT_APP_API_URL = "https://backend-checker-bo4xf3yfha-ew.a.run.app"
-  }
-
-  deployment {
-    zip {
-      source_url = "https://storage.googleapis.com/${google_storage_bucket.app.name}/${google_storage_bucket_object.app.name}"
-    }
-  }
-
-  instance_class = "F4"
-
-  automatic_scaling {
-    max_concurrent_requests = 10
-    min_idle_instances      = 1
-    max_idle_instances      = 3
-    min_pending_latency     = "1s"
-    max_pending_latency     = "5s"
-    standard_scheduler_settings {
-      target_cpu_utilization        = 0.5
-      target_throughput_utilization = 0.75
-      min_instances                 = 0
-      max_instances                 = 4
-    }
-  }
-  noop_on_destroy = true
-  delete_service_on_destroy = true
-}
+# resource "google_app_engine_standard_app_version" "latest_version" {
+#
+#   version_id = 1
+#   service    = "default"
+#   runtime    = "nodejs20"
+#   project= var.project_id
+#
+#   entrypoint {
+#     shell = " npm start"
+#   }
+#   env_variables = {
+#     REACT_APP_API_URL = "https://backend-checker-bo4xf3yfha-ew.a.run.app"
+#   }
+#
+#   deployment {
+#     zip {
+#       source_url = "https://storage.googleapis.com/${google_storage_bucket.app.name}/${google_storage_bucket_object.app.name}"
+#     }
+#   }
+#
+#   instance_class = "F4"
+#
+#   automatic_scaling {
+#     max_concurrent_requests = 10
+#     min_idle_instances      = 1
+#     max_idle_instances      = 3
+#     min_pending_latency     = "1s"
+#     max_pending_latency     = "5s"
+#     standard_scheduler_settings {
+#       target_cpu_utilization        = 0.5
+#       target_throughput_utilization = 0.75
+#       min_instances                 = 0
+#       max_instances                 = 4
+#     }
+#   }
+#   noop_on_destroy = true
+#   delete_service_on_destroy = true
+# }
